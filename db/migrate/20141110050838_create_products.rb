73c1e3ac7824032e04fb0bd629f41b0d8a07c592
shaynaCummings <shayna.cummings@gmail.com>
2014-11-10 00:11:25 -0500

    ran rails g model for Product and Category; edited migration file such that product belongs_to category

---
+++ db/migrate/20141110050838_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.decimal :price
+      t.belongs_to :category
+
+      t.timestamps null: false
+    end
+  end
+end
