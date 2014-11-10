74892e3c9fd8201e81a02bb8bfa29ca9f9730eb9
shaynaCummings <shayna.cummings@gmail.com>
2014-11-10 11:11:41 -0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
+category1 = Category.create(name: 'Catering Menu')
+category2 = Category.create(name: 'Sandwiches')
+category3 = Category.create(name: 'Beverages')
+
+
+products = Product.create([{name: 'The Somerville', price:6.75, category_id:2},
+{name: 'Small Bagel Box', price:12.00, category_id:1},
+{name: 'The Brookline', price:7.50, category_id:2}, ])
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
