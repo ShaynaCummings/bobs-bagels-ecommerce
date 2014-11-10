160a9c71e8fdffe48f8d4714a7ea7e9e04ccf682
shaynaCummings <shayna.cummings@gmail.com>
2014-11-10 11:04:38 -0500

    added show/index actions in controllers and routes for Products and Categories

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,13 @@
 class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+    render json: @products
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    render json: @product
+  end
+
 end
