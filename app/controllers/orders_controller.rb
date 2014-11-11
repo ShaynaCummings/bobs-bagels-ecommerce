f42678946ad797876a0a5d1e68a1b84116918cc8
George <gbanis@gmail.com>
2014-11-11 15:02:33 -0500

    Added sample order object

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,27 +2,31 @@ class OrdersController < ApplicationController
 
   def create
 
-    # order = {
-    #   lineitems:[
-    #     # The Hub with Everything
-    #     {
-    #       product_id: 4,
-    #       combined_price: 6,
-    #       line_item_options: [14, 2, 7, 15]
-    #     },
-    #     # Latte small
-    #     {
-    #       product_id: 10,
-    #       combined_price: 3.5,
-    #       line_item_options: [45]
-    #     }
-    #   ]
-    #   status: 'pending',
-    #   street_address: '50 Melcher Street',
-    #   zip_code: '02210',
-    #   delivery_price: 6,
-    #   order_total: 15.5
-    # }
+
+# order = {
+#       lineitems:[
+#         # The Hub with Everything
+#         {
+#           product_id: 4,
+#           combined_price: 6,
+#           line_item_options: [14, 2, 7, 15]
+#         },
+#         # Latte small
+#         {
+#           product_id: 10,
+#           combined_price: 3.5,
+#           line_item_options: [45]
+#         }
+#       ],
+#       status: 'pending',
+#       street_address: '50 Melcher Street',
+#       city: 'Boston',
+#       state: 'MA',
+#       zip_code: '02210',
+#       delivery_price: 6,
+#       order_total: 15.5
+#     }
+
 
     @order = Order.create(params[:order])
 
