adab27c401946de54628f82dffadb0943cae06b1
George <gbanis@gmail.com>
2014-11-11 11:19:55 -0500

    Changed the prices in migrations from decimal to numeric

--- db/migrate/20141110052817_create_options.rb
+++ db/migrate/20141110052817_create_options.rb
@@ -2,7 +2,7 @@ class CreateOptions < ActiveRecord::Migration
   def change
     create_table :options do |t|
       t.string :name
-      t.decimal :price
+      t.numeric :price
 
       t.timestamps null: false
     end
--- db/migrate/20141110052953_create_lineitems.rb
+++ db/migrate/20141110052953_create_lineitems.rb
@@ -1,7 +1,7 @@
 class CreateLineitems < ActiveRecord::Migration
   def change
     create_table :lineitems do |t|
-      t.decimal :combined_price
+      t.numeric :combined_price
       t.belongs_to :product
       t.belongs_to :order
 
