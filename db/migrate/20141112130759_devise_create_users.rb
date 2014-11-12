0a3b1fa4a1c69b1276bd2683de3491caccea3759
areddy14 <areddy1441@gmail.com>
2014-11-12 08:12:42 -0500

    I got rid of my earlier migration and added the relevant attributes to the ...devise_create_users migration instead.

--- db/migrate/20141112130759_devise_create_users.rb
+++ db/migrate/20141112130759_devise_create_users.rb
@@ -1,6 +1,11 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :street
+      t.string :city
+      t.string :zip
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
