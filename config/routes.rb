3c9cfa8d655cacffc17ef20b60b92184c6add323
shaynaCummings <shayna.cummings@gmail.com>
2014-11-11 23:19:29 -0500

    added routes for charges

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:show, :index, :create]
 
+  resources :charges
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
