bebc124ab502d04e9d636d2e39fe012672b661a2
shaynaCummings <shayna.cummings@gmail.com>
2014-11-12 14:55:03 -0500

    fixing merge issue?

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,23 +1,29 @@
 class ChargesController < ApplicationController
 
-  def new
+  def show
+    render json: @charge
   end
 
+
   def create
+  Stripe.api_key = ENV['PUBLISHABLE_KEY']
+
+    @charge = Charge.new(params[:id])
+
     # Amount in cents
-    @amount = 500
+    # @amount = 500
 
-    customer = Stripe::Customer.create(
-      :email => 'example@stripe.com',
-      :card  => params[:stripeToken]
-    )
+    # customer = Stripe::Customer.create(
+    #   :email => 'example@stripe.com',
+    #   :card  => params[:stripeToken]
+    # )
 
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
-    )
+    # charge = Stripe::Charge.create(
+    #   :customer    => customer.id,
+    #   :amount      => @amount,
+    #   :description => 'Rails Stripe customer',
+    #   :currency    => 'usd'
+    # )
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
